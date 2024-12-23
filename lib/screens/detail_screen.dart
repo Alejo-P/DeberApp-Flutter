ea55e93a34846ce7f6f7ff30229c74f3c3b7dac0
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-12-23 01:04:04 -0500

    Añadir pantalla de detalles para mostrar información del contacto seleccionado

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({
+    super.key,
+    required this.contact,
+  });
+
+  // Contacto seleccionado
+  final Map<String, String> contact;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
